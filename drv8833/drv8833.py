f15ff257c8c02780c8a8d211da78442521395811
harry-upton <harryupton100@gmail.com>
2025-03-03 00:40:46 +0000

    gpio and motor control functions

--- drv8833/drv8833.py
+++ drv8833/drv8833.py
@@ -9,11 +9,14 @@ Datasheet: https://www.ti.com/lit/ds/symlink/drv8833.pdf
 
 Implementation Notes
 --------------------
-
+- After the class has been initialised, you must first call configure_gpio()
+- Motor speed goes from 0 -> -255 to 255
+- Call cleanup() when you are done
 
 Missing Features (ToDo):
 ------------------------
-
+- Set PWM frequency
+- Ability to change motor default direction
 
 """
 import pigpio
@@ -26,43 +29,55 @@ class DRV8833:
                  PIN_AIN2: int,
                  PIN_BIN1: int,
                  PIN_BIN2: int,
-                 PIN_SLEEP: int,
-                 PIN_FAULT: int,
-                 FREQUENCY: float = 100):
+                 PIN_nSLEEP: int,
+                 PIN_nFAULT: int):
         self.PIN_AIN1 = PIN_AIN1
         self.PIN_AIN2 = PIN_AIN2
         self.PIN_BIN1 = PIN_BIN1
         self.PIN_BIN2 = PIN_BIN2
-        self.PIN_SLEEP = PIN_SLEEP
-        self.PIN_FAULT = PIN_FAULT
+        self.PIN_nSLEEP = PIN_nSLEEP
+        self.PIN_nFAULT = PIN_nFAULT
         self.freq = FREQUENCY
 
     def configure_gpio(self):
-        pass
+        self.pi = pigpio.pi()
+        self.pi.set_mode(self.PIN_AIN1, pigpio.OUTPUT)
+        self.pi.set_mode(self.PIN_AIN2, pigpio.OUTPUT)
+        self.pi.set_mode(self.PIN_BIN1, pigpio.OUTPUT)
+        self.pi.set_mode(self.PIN_BIN2, pigpio.OUTPUT)
 
+        self.pi.set_mode(self.PIN_nSLEEP, pigpio.OUTPUT)
+        self.pi.write(self.PIN_nSLEEP, 1) # Enable the chip
+        self.pi.set_mode(self.PIN_nFAULT, pigpio.INPUT)
 
-    def configure_pwm(self):
-        pass
-
-
-    def set_motorA_speed(self, speed: float):
+    def set_motorA_speed(self, speed: int):
+        if speed > 255 or speed < -255:
+            raise ValueError(f"Speed ({speed}) is outside of suitable range (-255 -> +255)")
         if speed >= 0:
-            pass
+            self.pi.set_PWM_dutycycle(self.PIN_AIN1, speed)
+            self.pi.set_PWM_dutycycle(self.PIN_AIN2, 0)
         else:
-            pass
+            self.pi.set_PWM_dutycycle(self.PIN_AIN1, 0)
+            self.pi.set_PWM_dutycycle(self.PIN_AIN2, -speed)
+
     def set_motorB_speed(self, speed: int):
+        if speed > 255 or speed < -255:
+            raise ValueError(f"Speed ({speed}) is outside of suitable range (-255 -> +255)")
         if speed >= 0:
-            pass
+            self.pi.set_PWM_dutycycle(self.PIN_BIN1, speed)
+            self.pi.set_PWM_dutycycle(self.PIN_BIN2, 0)
         else:
-            pass
+            self.pi.set_PWM_dutycycle(self.PIN_BIN1, 0)
+            self.pi.set_PWM_dutycycle(self.PIN_BIN2, -speed)
 
     def set_sleep(self, sleep_state: bool):
         if type(sleep_state) != bool:
             raise TypeError("sleep_state not a boolean value")
-        pass
+        self.pi.write(self.PIN_nSLEEP, int((not sleep_state))) # Pin is nSleep, write LOW to disable chip
 
     def get_fault(self) -> bool:
-        return not GPIO.input(self.PIN_FAULT)
+        return not bool(self.pi.read(self.PIN_nFAULT)) # Pin is nFault, reads LOW when there is a fault.
     
     def cleanup(self):
-        pass
+        self.pi.stop()
+        
